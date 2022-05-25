cb0d6cc2689a99fb446a00eb3cff7a08687829e3
Ali Mostafa <ali.farhangiyan@gmail.com>
2022-05-26 00:04:37 +0430

    configuration connection string for connect to db

--- WebUI/Program.cs
+++ WebUI/Program.cs
@@ -1,8 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<DatabaseContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration["ConnectionStrings:SqlServerConnection"]);
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
