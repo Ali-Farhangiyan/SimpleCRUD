74105775df73e6c4466b0e04eb1ff853e2421039
Ali Mostafa <ali.farhangiyan@gmail.com>
2022-05-25 23:58:25 +0430

    Add project files.

---
+++ Persistence/Context/DatabaseContext.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entites;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Context
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Person> People { get; set; } = null!;
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+    }
+}
